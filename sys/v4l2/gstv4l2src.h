34f2bf3ba2d02ecf662bbda36e4eb323f89c5893
Thao Luong <thao.luong.uw@rvc.renesas.com>
2015-12-20 13:31:57 +0700

    v4l2src: Set default output resolution equal with input resolution
    Normally, when do not specify output resolution, output resolution
    will be fixed 320x200.
    This patch file will be fixed output resolution equal input resolution
    instead of 320x200. Information of input resolution will be got through
    CROPCAP callback.
    
    refs #82645
    Signed-off-by: Thao Luong <thao.luong.uw@rvc.renesas.com>

--- sys/v4l2/gstv4l2src.h
+++ sys/v4l2/gstv4l2src.h
@@ -2,6 +2,7 @@
  *
  * Copyright (C) 2001-2002 Ronald Bultje <rbultje@ronald.bitfreak.net>
  *               2006 Edgard Lima <edgard.lima@indt.org.br>
+ * Copyright (C) 2015, Renesas Electronics Corporation
  *
  * gstv4l2src.h: BT8x8/V4L2 source element
  *
@@ -60,6 +61,7 @@ struct _GstV4l2Src
   guint64 offset;
 
   GstClockTime ctrl_time;
+  struct v4l2_rect in_size;
 };
 
 struct _GstV4l2SrcClass
