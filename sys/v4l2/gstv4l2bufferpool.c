b5981b4d58774bca5efacb22f1ba6618834868ca
Shinya Saito <ssaito@igel.co.jp>
2019-01-07 16:10:09 +0900

    v4l2src: Support option no-resurect-buf
    no-resurect-buf option used incase v4l2src and downstream share memory.
    Enable this option to skip creating new buffer when all buffers in queue of
    v4l2src is used up, lost or reach to threshold value.
    This option is a workaround incase using v4l2src (dmabuf/userptr) and
    omxh264enc (dmabuf/no-copy) which cannot change number of allocated buffer.
    
    Based on the original patch
    Title: v4l2src: Support option no-resurect-buf
    Author: Thao Luong <thao.luong.xh@renesas.com>

--- sys/v4l2/gstv4l2bufferpool.c
+++ sys/v4l2/gstv4l2bufferpool.c
@@ -770,8 +770,13 @@ gst_v4l2_buffer_pool_streamon (GstV4l2BufferPool * pool)
          * so the driver don't underflow immediatly. As we have put then back
          * into the base class queue, resurect them, then releasing will queue
          * them back. */
-        while (gst_v4l2_buffer_pool_resurect_buffer (pool) == GST_FLOW_OK)
-          continue;
+        if (GST_IS_V4L2SRC (pool->obj->element) &&
+                    GST_V4L2SRC (pool->obj->element)->no_resurect_buf == TRUE) {
+          GST_DEBUG_OBJECT (pool, "Skip buffer resurection");
+        } else {
+          while (gst_v4l2_buffer_pool_resurect_buffer (pool) == GST_FLOW_OK)
+            continue;
+        }
       }
 
       if (obj->ioctl (pool->video_fd, VIDIOC_STREAMON, &obj->type) < 0)
@@ -1413,8 +1418,14 @@ gst_v4l2_buffer_pool_acquire_buffer (GstBufferPool * bpool, GstBuffer ** buffer,
 
   /* If this is being called to resurect a lost buffer */
   if (params && params->flags & GST_V4L2_BUFFER_POOL_ACQUIRE_FLAG_RESURRECT) {
-    ret = pclass->acquire_buffer (bpool, buffer, params);
-    goto done;
+    if (GST_IS_V4L2SRC (obj->element) &&
+        GST_V4L2SRC (obj->element)->no_resurect_buf == TRUE) {
+      GST_WARNING_OBJECT (pool,
+          "Lost one buffer but skipped resurrect for this buffer");
+    } else {
+      ret = pclass->acquire_buffer (bpool, buffer, params);
+      goto done;
+    }
   }
 
   switch (obj->type) {
@@ -2021,9 +2032,18 @@ gst_v4l2_buffer_pool_process (GstV4l2BufferPool * pool, GstBuffer ** buf)
             /* If we have no more buffer, and can allocate it time to do so */
             if (num_queued == 0) {
               if (GST_V4L2_ALLOCATOR_CAN_ALLOCATE (pool->vallocator, MMAP)) {
-                ret = gst_v4l2_buffer_pool_resurect_buffer (pool);
-                if (ret == GST_FLOW_OK)
+                if (GST_IS_V4L2SRC (pool->obj->element) &&
+                    GST_V4L2SRC (pool->obj->element)->no_resurect_buf == TRUE) {
+                  GST_V4L2SRC (pool->obj->element)->skipped_buffer++;
+                  GST_WARNING_OBJECT (pool,
+                      "Used up buffer in queued, skipped resurrect %d buffers",
+                      GST_V4L2SRC (pool->obj->element)->skipped_buffer);
                   goto done;
+                } else {
+                  ret = gst_v4l2_buffer_pool_resurect_buffer (pool);
+                  if (ret == GST_FLOW_OK)
+                    goto done;
+                }
               }
             }
 
@@ -2032,9 +2052,18 @@ gst_v4l2_buffer_pool_process (GstV4l2BufferPool * pool, GstBuffer ** buf)
               GstBuffer *copy;
 
               if (GST_V4L2_ALLOCATOR_CAN_ALLOCATE (pool->vallocator, MMAP)) {
-                ret = gst_v4l2_buffer_pool_resurect_buffer (pool);
-                if (ret == GST_FLOW_OK)
+                if (GST_IS_V4L2SRC (pool->obj->element) &&
+                    GST_V4L2SRC (pool->obj->element)->no_resurect_buf == TRUE) {
+                  GST_V4L2SRC (pool->obj->element)->skipped_buffer++;
+                  GST_WARNING_OBJECT (pool,
+                      "Reached threshold, skipped resurrect %d buffers",
+                      GST_V4L2SRC (pool->obj->element)->skipped_buffer);
                   goto done;
+                } else {
+                  ret = gst_v4l2_buffer_pool_resurect_buffer (pool);
+                  if (ret == GST_FLOW_OK)
+                    goto done;
+                }
               }
 
               /* copy the buffer */
